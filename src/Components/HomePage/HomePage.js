a7394eea161400290cb0a6073a9eb452aff5fad1
Jamie <phillipsja97@gmail.com>
2020-02-12 13:34:22 -0600

    homepage halfway done

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -5,12 +5,15 @@ class HomePage extends React.Component {
   render() {
     return (
     <div className="HomePage">
-      <div className="golf"></div>
-      <div className="home">
-Scroll Up and Down this page to see the parallax scrolling effect.
-This div is just here to enable scrolling.
-Tip: Try to remove the background-attachment property to remove the scrolling effect.
-</div>
+      <div className="fluid-container">
+        <div class="fluid-container" id="homeScreen">
+        </div>
+      </div>
+      <div class="fluid-container" id="aboutMe">
+        <div className="title justify-content-start">
+          <p>jamiephillips <span>~/about-me/</span> (master):</p>
+        </div>
+      </div>
     </div>
     );
   }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,12 +5,12 @@ import './NavBar.scss';
 class NavBar extends React.Component {
   render() {
     return (
-      <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#homePage">Jamie Phillips</Navbar.Brand>
+      <Navbar sticky="top" collapseOnSelect expand="lg" bg="light" variant="light">
+  <Navbar.Brand href=".homeScreen">Jamie Phillips</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
     <Nav>
-      <Nav.Link href="#homePage">About Me</Nav.Link>
+      <Nav.Link href="#aboutMe">About Me</Nav.Link>
       <Nav.Link href="#tech">Technologies</Nav.Link>
       <Nav.Link href="#projects">Projects</Nav.Link>
     </Nav>
