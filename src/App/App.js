b762b3e3cc9fb2ca031c18b126e408c839d3ae74
Jamie <phillipsja97@gmail.com>
2020-02-12 00:53:09 -0600

    parallax figured out. will revisit

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from ''
+import HomePage from '../Components/HomePage/HomePage';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './HomePage.scss';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
+    <div className="HomePage">
+      <div className="golf"></div>
+      <div className="home">
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+    </div>
     );
   }
 }
