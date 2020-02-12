d3c106ba319b1af3c4d2c0c0cfc2e6a49ac4f2c1
Jamie <phillipsja97@gmail.com>
2020-02-12 15:40:00 -0600

    updated folder structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import HomePage from '../Components/HomePage/HomePage';
-import NavBar from '../Components/NavBar/NavBar';
+import HomePage from '../Components/Pages/HomePage/HomePage';
+import NavBar from '../Components/Shared/NavBar/NavBar';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
