ee3baaf41ba7ca93f4cc20029ea940c6b66c0cb9
Jamie <phillipsja97@gmail.com>
2020-04-05 16:54:36 -0500

    last fix

--- src/Components/Pages/About/AboutTablet.js
+++ src/Components/Pages/About/AboutTablet.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
 import './About.scss';
--- src/Components/Pages/HomePage/HomePage.js
+++ src/Components/Pages/HomePage/HomePage.js
@@ -9,7 +9,7 @@ class HomePage extends React.Component {
       <Media queries={{
         small: '(max-width: 599px)',
         medium: '(min-width: 600px) and (max-width: 1199px)',
-        large: '(min-width: 1200px)'
+        large: '(min-width: 1450px)',
       }}>
         {(matches) => (
           <Fragment>
