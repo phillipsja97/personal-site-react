77b2cdff467460d72c5b0fd4081ea53e49ac06a5
Jamie <phillipsja97@gmail.com>
2020-04-05 16:39:08 -0500

    dynamic fix

--- src/Components/Pages/HomePage/HomePage.js
+++ src/Components/Pages/HomePage/HomePage.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import MediaQuery from 'react-responsive';
+import React, { Fragment } from 'react';
+import Media from 'react-media';
 import './HomePage.scss';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="Homepage">
-        <h1>Hi</h1>
-      </div>
+      <div>
+      <Media queries={{
+        small: '(max-width: 599px)',
+        medium: '(min-width: 600px) and (max-width: 1199px)',
+        large: '(min-width: 1200px)'
+      }}>
+        {(matches) => (
+          <Fragment>
+            {matches.small && <p>I am small!</p>}
+            {matches.medium && <p>I am medium!</p>}
+            {matches.large && <p>I am large!</p>}
+          </Fragment>
+        )}
+      </Media>
+    </div>
     );
   }
 }
