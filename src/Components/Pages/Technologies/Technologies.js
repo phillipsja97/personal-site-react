bfcaffcadb32904236348081868263951e3e4390
Jamie <phillipsja97@gmail.com>
2020-04-05 19:22:46 -0500

    tech has been set up for mobile and tablets

--- src/Components/Pages/Technologies/Technologies.js
+++ src/Components/Pages/Technologies/Technologies.js
@@ -1,10 +1,8 @@
 /* eslint-disable max-len */
-import React from 'react';
-import MediaQuery from 'react-responsive';
+import React, { Fragment } from 'react';
+import Media from 'react-media';
 import Zoom from 'react-reveal/Zoom';
-import Background from '../HomePage/HomePage';
 import TechnologiesCard from '../../Shared/TechnologiesCard/TechnologiesCard';
-import techData from '../../../Helpers/data/techData';
 import './Technologies.scss';
 
 class Technologies extends React.Component {
@@ -16,18 +14,32 @@ class Technologies extends React.Component {
     const { technologies } = this.props;
     return (
       <React.Fragment className="fluid-container">
-        <div className="technologies">
                 <div className="images" id="tech">
-                  <p className="techTitle"> jamiephillips<span> ~/technologies-I-have-used/</span><span2> (master):</span2></p>
+                  <Media queries={{
+                    small: '(min-width: 320px) and (max-width: 767px)',
+                    medium: '(min-width: 768px) and (max-width: 1024px)',
+                    large: '(min-width: 1023px)',
+                  }}>
+                    {(matches) => (
+                      <Fragment>
+                        {matches.small && <p className="techTitleMob"> jamiephillips<span className="spanMobile"> ~/technologies-I-have-used/</span><span2> (master):</span2></p>}
+                        {matches.medium && <p className="techTitleTab"> jamiephillips<span className="spanTablet"> ~/technologies-I-have-used/</span><span2> (master):</span2></p>}
+                        {matches.large && <p className="techTitle"> jamiephillips<span> ~/technologies-I-have-used/</span><span2> (master):</span2></p>}
+                      </Fragment>
+                    )}
+                  </Media>
                   <Zoom cascade>
-                      <div className="empty">
-                        <div className="techArea">
-                            { technologies.map((tech) => <TechnologiesCard key={tech.id} tech={tech} />)};
+                    <div className="empty">
+                      <div className="techArea">
+                        <div className="empty">
+                          <div className="techArea">
+                              { technologies.map((tech) => <TechnologiesCard key={tech.id} tech={tech} />)};
+                          </div>
                         </div>
                       </div>
-                    </Zoom>
+                    </div>
+                  </Zoom>
                 </div>
-        </div>
       </React.Fragment>
     );
   }
