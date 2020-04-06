746918a38a4e86f5be11eddafc7e8357952684fd
Jamie <phillipsja97@gmail.com>
2020-04-06 14:57:34 -0500

    added new MaterialUI card, still needs to style

--- src/Components/Shared/ProjectCard/ProjectCard.js
+++ src/Components/Shared/ProjectCard/ProjectCard.js
@@ -1,41 +1,161 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { IconContext } from 'react-icons';
-import { FaGithub, FaGlobe } from 'react-icons/fa';
-import './ProjectCard.scss';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import projectData from '../../../Helpers/data/projectData';
 
-class ProjCard extends React.Component {
-  render() {
-    const { project } = this.props;
-    return (
-      <Card className="bg-dark text-white projectCard">
-        <Card.Img src={project.screenshot} alt="Card Image" className="screenshotOverlay" />
-          <Card.ImgOverlay>
-           { (project.title.includes('Capstone')) ? (<Card.Title className="text-center projTitle"><span3>{project.title}</span3></Card.Title>)
-             : (<Card.Title className="text-center projTitle">{project.title}</Card.Title>)
-           }
-            <Card.Text className="text-center projText">
-              {project.description}
-            </Card.Text>
-            <Card.Text className="text-center projTechUsed">
-              <span2>Technologies Used: </span2>{project.technologiesUsed}
-            </Card.Text>
-              <Card.Text className="d-inline-flex icons">
-                <IconContext.Provider value={{ color: 'rgb(19, 228, 228)', className: 'githubIcon' }}>
-                  <div>
-                    <a href={project.githubUrl}><FaGithub /></a>
-                  </div>
-                </IconContext.Provider>
-                <IconContext.Provider value={{ color: 'rgb(19, 228, 228)', className: 'webIcon' }}>
-                  <div>
-                    <a href={project.url}><FaGlobe /></a>
-                  </div>
-                </IconContext.Provider>
-              </Card.Text>
-          </Card.ImgOverlay>
-      </Card>
-    );
-  }
+const getProjects = () => {
+  projectData.getProjects()
+    .then((projects) => {
+    })
+    .catch((errorFromProj) => console.error(errorFromProj));
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function ProjCard(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+           J
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.project.title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.project.screenshot}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
 
-export default ProjCard;
+
+/* <Card className="bg-dark text-white projectCard">
+                                    <Card.Img src={project.screenshot} alt="Card Image" className="screenshotOverlay" />
+                                      <Card.ImgOverlay>
+                                      { (project.title.includes('Capstone')) ? (<Card.Title className="text-center projTitle"><span3>{project.title}</span3></Card.Title>)
+                                        : (<Card.Title className="text-center projTitle">{project.title}</Card.Title>)
+                                      }
+                                        <Card.Text className="text-center projText">
+                                          {project.description}
+                                        </Card.Text>
+                                        <Card.Text className="text-center projTechUsed">
+                                          <span2>Technologies Used: </span2>{project.technologiesUsed}
+                                        </Card.Text>
+                                          <Card.Text className="d-inline-flex icons">
+                                            <IconContext.Provider value={{ color: 'rgb(19, 228, 228)', className: 'githubIcon' }}>
+                                              <div>
+                                                <a href={project.githubUrl}><FaGithub /></a>
+                                              </div>
+                                            </IconContext.Provider>
+                                            <IconContext.Provider value={{ color: 'rgb(19, 228, 228)', className: 'webIcon' }}>
+                                              <div>
+                                                <a href={project.url}><FaGlobe /></a>
+                                              </div>
+                                            </IconContext.Provider>
+                                          </Card.Text>
+                                      </Card.ImgOverlay>
+                                  </Card> */
