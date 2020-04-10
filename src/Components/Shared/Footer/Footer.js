b79f38ba224569f3334240a4d08b8d27c38de8f7
Jamie <phillipsja97@gmail.com>
2020-04-09 22:17:26 -0500

    change

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer d-flex justify-content-center">
-          <p>&#169; Jamie Phillips 2020.</p>
+          <p>&#169; Jamie Phillips 2020. Powered By React.</p>
       </div>
     );
   }
